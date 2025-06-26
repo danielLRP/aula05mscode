897c04158eef25d6198e1c332874f1a114773d75
Daniel Lima <dladaniellima1@gmail.com>
2025-06-26 04:03:59 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+var number1 = document.getElementById("n1")
+var number2 = document.getElementById("n2")
+var resultado = document.getElementById('resultado')
+
+function soma(number1, number2){
+    resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value);
+
+}
+
+function subtracao(number1, number2){
+    resultado.innerHTML = parseInt(n1.value) - parseInt(n2.value);
+}
